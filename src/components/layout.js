fcb059dce0a5a184cf66daa3453af61d59d1dc0a
jayjadhav96 <jay45.jadhav@gmail.com>
2021-07-20 12:19:55 +0530

    Header component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import Header from "./header"
+import Header from "./Header"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
